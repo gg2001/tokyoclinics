a48a2d428fed84b9297e604bbd0a0df552ff63ba
Gautham <gautham.gg@outlook.com>
2016-07-07 20:45:39 +0900

    Configured database-NO PROBLEMs

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
 
 Template.list.onCreated(function listOnCreated() {
@@ -8,7 +7,9 @@ Template.list.onCreated(function listOnCreated() {
 });
 
 Template.list.helpers({
-
+	'clinic': function(){
+        return Clinics.find();
+    }
 });
 
 Template.list.events({
