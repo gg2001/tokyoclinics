858b8fabcbee664bc7884e587cd3113e1a34a87b
Gautham <gautham.gg@outlook.com>
2016-07-08 14:38:59 +0900

    auth

--- client/main.js
+++ client/main.js
@@ -2,6 +2,14 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
+Template.main.events({
+	'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('/');
+    }
+});
+
 Template.list.onCreated(function listOnCreated() {
 
 });
@@ -14,4 +22,33 @@ Template.list.helpers({
 
 Template.list.events({
 
+});
+
+Template.register.events({
+    'submit form': function(){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
+        Router.go('/');
+    }
+});
+
+Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email, password, function(error){
+            if(error){
+        console.log(error.reason);
+        document.getElementById("err").innerHTML = "Incorrect email or password";
+    } else {
+        Router.go('/');
+    }
+		});
+    }
 });
