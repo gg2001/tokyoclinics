9bfa4155c8e8afad494e40c002a96f68c45ddb4d
Gautham <gautham.gg@outlook.com>
2016-07-09 19:15:31 +0900

    twitter

--- client/otherlogin/otherlogin.js
+++ client/otherlogin/otherlogin.js
@@ -2,3 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './otherlogin.html';
 import '../../imports/ui/style.css'
+
+Template.page.rendered = function() {
+	document.getElementById("backButton").innerHTML = '<a href="../login"><span class="glyphicon glyphicon-chevron-left"></span> Back</a>';
+}
